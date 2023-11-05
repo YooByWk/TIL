8d1af4850b702682161527b4c77bf33350cd085e
yoo <tls1919@naver.com>
2023-11-06 00:11:01 +0900

    prettier

--- 2023_10_Oct/1019_DRF_N_1/10-02-django-rest-framework/articles/serializers.py
+++ 2023_10_Oct/1019_DRF_N_1/10-02-django-rest-framework/articles/serializers.py
@@ -17,7 +17,9 @@ class CommentSerializer(serializers.ModelSerializer):
             fields = ('title',)
 
     # 오버라이드 할 것입니다.
+    #################################################
     article = ArticleSerializer(read_only=True) # 아래의 코드는 문법적 오류가 발생
+    #################################################
     # modelform과 비슷한것입니다.
     class Meta:
         model = Comment 
@@ -28,7 +30,7 @@ class CommentSerializer(serializers.ModelSerializer):
 class ArticleSerializer(serializers.ModelSerializer):
     # 1 -> N 참조 중 // 댓글이 몇개건 다중 데이터가 나옴
     # 읽기 전용으로 변경할 것 / 이름 변경 불가능 / 역참조 매니저 이름 따라감
-    comment_set = CommentSerializer(many=True, read_only=True) 
+    comment_set = CommentSerializer(many=True, read_only=True)
     
     # 댓글 수 만큼 출력해주는 것
     # ArticleSerializer 는 article model에 기반해서 만들어집니다.
